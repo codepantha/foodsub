56b2f85c250793986e9d817b63940dbb4ff61d72
codepanther <pezechigozirim@gmail.com>
2021-12-09 23:33:39 +0100

    Create About us section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import { Navbar } from './components';
-import { Header } from './containers';
+import { About, Header } from './containers';
 
 const App = () => (
   <div className="App">
     <Navbar />
     <Header />
+    <About />
   </div>
 );
 
---
+++ src/containers/about/About.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Button } from "../../components";
+import "./About.css";
+import plate from "../../assets/plate.svg";
+
+const About = () => {
+  return (
+    <section className="foodsub__about">
+      <h2>About Us</h2>
+      <div className="foodsub__about-content">
+        <div className="foodsub__about-content_text">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nec
+            suscipit diam mi risus et tincidunt sollicitudin <br /><br /> egestas
+            commodo. Non neque odio in nulla in et. Ultricies porttitor egesta.
+          </p>
+          <Button text="Learn More" height='60px' width='241px' />
+        </div>
+        <div className="foodsub__about-content_img">
+          <img src={plate} alt='plate' />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
