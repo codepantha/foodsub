ea9ba5c77ef3d616159eb13d5d6fb776ab90ec37
codepanther <pezechigozirim@gmail.com>
2021-12-09 20:03:35 +0100

    Create the navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import './App.css'
+import './App.css';
 import { Navbar } from './components';
 
-const App = () => {
-  return (
-    <div className="App">
-      <Navbar />
-    </div>
-  )
-}
+const App = () => (
+  <div className="App">
+    <Navbar />
+  </div>
+);
 
 export default App;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
-import './Navbar.css';
+import React from "react";
+import "./Navbar.css";
+import logo from "../../assets/logo.svg";
 
 const Navbar = () => {
   return (
-    <div>
-      NAVBAR
-    </div>
-  )
-}
+    <nav className="foodsub__navbar">
+      <div className="foodsub__navbar-logo">
+        <img src={logo} />
+      </div>
+      <div className="foodsub__navbar-navitems">
+        <ul className="foodsub__navbar-navitems_links">
+          <li>Home</li>
+          <li>About</li>
+          <li>Services</li>
+          <li>Contact</li>
+          <li>FAQ</li>
+        </ul>
+        <div className="foodsub__navbar-navitems_buttons">
+          <button>Login</button>
+          <button>Register</button>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
