f725215fc51c7bcb5a5cbdc65baeeaa3db1eb0aa
codepanther <pezechigozirim@gmail.com>
2021-12-13 00:36:31 +0100

    Create the call to action

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import "tachyons";
 import './App.css';
-import { Navbar } from './components';
+import { CTA, Navbar } from './components';
 import { About, Header, HIW, Services } from './containers';
 
 const App = () => (
@@ -11,6 +11,7 @@ const App = () => (
     <About />
     <Services />
     <HIW />
+    <CTA />
   </div>
 );
 
--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Button } from '..';
 import './CTA.css';
 
 const CTA = () => {
   return (
-    <div>
-      CTA
-    </div>
+    <section className="foodsub__cta">
+      <h2>Waitlist</h2>
+      <div className="foodsub__cta-input_fields">
+        <input type="email" placeholder="Email Address" />
+        <Button text="Join the waitlist" width="335px" />
+      </div>
+    </section>
   )
 }
 
