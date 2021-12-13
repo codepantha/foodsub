5ad8995c55d3d06d51874c6f1fe1e9856f795b14
codepanther <pezechigozirim@gmail.com>
2021-12-13 13:22:23 +0100

    Creat the footer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import "tachyons";
 import './App.css';
 import { CTA, Navbar } from './components';
-import { About, FAQS, Header, HIW, Services } from './containers';
+import { About, FAQS, Footer, Header, HIW, Services } from './containers';
 
 const App = () => (
   <div className="App">
@@ -13,6 +13,7 @@ const App = () => (
     <HIW />
     <CTA />
     <FAQS />
+    <Footer />
   </div>
 );
 
