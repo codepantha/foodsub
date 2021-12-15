55a96807b0ec334732254a815c86996455ff33e7
codepanther <pezechigozirim@gmail.com>
2021-12-16 00:35:53 +0100

    Responsive navbar with animation

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,27 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import { Button } from "..";
 import logo from "../../assets/logo.svg";
+import { RiCloseLine, RiMenu2Line } from "react-icons/ri";
+
+const Menu = () => (
+  <>
+    <ul className="foodsub__navbar-navitems_links">
+      <li>
+        <a href="#header" className="link dim">
+          Home
+        </a>
+      </li>
+      <li>
+        <a href="#about" className="link dim">
+          About
+        </a>
+      </li>
+      <li>
+        <a href="#services" className="link dim">
+          Services
+        </a>
+      </li>
+      <li>
+        <a href="#contact" className="link link:hover dim">
+          Contact
+        </a>
+      </li>
+      <li>
+        <a href="#faq" className="link dim">
+          FAQ
+        </a>
+      </li>
+    </ul>
+    <div className="foodsub__navbar-navitems_buttons">
+      <Button text="Login" bgColor="#fff" color="#ffa927" />
+      <Button text="Register" color="#fff" />
+    </div>
+  </>
+);
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <nav className="foodsub__navbar">
       <div className="foodsub__navbar-logo">
         <img src={logo} />
       </div>
       <div className="foodsub__navbar-navitems">
-        <ul className="foodsub__navbar-navitems_links">
-          <li><a href="#header" className="link">Home</a></li>
-          <li><a href="#about" className="link">About</a></li>
-          <li><a href="#services" className="link">Services</a></li>
-          <li><a href="#contact" className="link link:hover">Contact</a></li>
-          <li><a href="#faq" className="link">FAQ</a></li>
-        </ul>
-        <div className="foodsub__navbar-navitems_buttons">
-          <Button text='Login' bgColor="#fff" color="#ffa927" />
-          <Button text='Register' color="#fff" />
-        </div>
+        <Menu />
       </div>
+      {toggleMenu ? (
+        <RiCloseLine size={20} onClick={() => setToggleMenu(false)} />
+      ) : (
+        <RiMenu2Line size={20} onClick={() => setToggleMenu(true)} />
+      )}
+
+      {toggleMenu && <nav className="nav__menu-container scale-up-hor-center"><Menu /></nav>}
     </nav>
   );
 };
